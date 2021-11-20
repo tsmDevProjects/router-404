3a802739097b18f253828a1be577e020bd255257
tsmsp <tsm_sp@hotmail.com>
2021-11-20 14:44:31 -0300

    home component

---
+++ src/component/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1>
+                Hello guys! Let's deal with 404?
+            </h1>
+        </div>
+    )
+}
+export default Home;
