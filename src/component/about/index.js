680ccbee8de9669cf50874b1f6043f9d08c4ab74
tsmsp <tsm_sp@hotmail.com>
2021-11-20 14:44:39 -0300

    about component

---
+++ src/component/about/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1>
+                About this example....
+            </h1>           
+        </div>
+    )
+}
+
+export default About;
+
